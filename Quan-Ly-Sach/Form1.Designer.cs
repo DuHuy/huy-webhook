8cf217cf1dfc014507e472d0e2f6ba644df92be4
huy <dungochuy21@gmail.com>
2024-03-24 02:25:25 +0700

    XML

---
+++ Quan-Ly-Sach/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace Quan_Ly_Sach
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnTim = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtmaS = new System.Windows.Forms.TextBox();
+            this.txttenS = new System.Windows.Forms.TextBox();
+            this.txtSL = new System.Windows.Forms.TextBox();
+            this.txtDG = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(193, 333);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(75, 23);
+            this.btnThem.TabIndex = 0;
+            this.btnThem.Text = "Them";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(283, 333);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(75, 23);
+            this.btnSua.TabIndex = 1;
+            this.btnSua.Text = "Sua";
+            this.btnSua.UseVisualStyleBackColor = true;
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(470, 333);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(75, 23);
+            this.btnXoa.TabIndex = 2;
+            this.btnXoa.Text = "Xoa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            // 
+            // btnTim
+            // 
+            this.btnTim.Location = new System.Drawing.Point(571, 333);
+            this.btnTim.Name = "btnTim";
+            this.btnTim.Size = new System.Drawing.Size(75, 23);
+            this.btnTim.TabIndex = 3;
+            this.btnTim.Text = "tim";
+            this.btnTim.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(112, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Mã sách";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(112, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Tên Sách";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(467, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Số Lượng";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(467, 151);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Đơn giá";
+            // 
+            // txtmaS
+            // 
+            this.txtmaS.Location = new System.Drawing.Point(193, 87);
+            this.txtmaS.Name = "txtmaS";
+            this.txtmaS.Size = new System.Drawing.Size(100, 20);
+            this.txtmaS.TabIndex = 8;
+            // 
+            // txttenS
+            // 
+            this.txttenS.Location = new System.Drawing.Point(193, 151);
+            this.txttenS.Name = "txttenS";
+            this.txttenS.Size = new System.Drawing.Size(100, 20);
+            this.txttenS.TabIndex = 9;
+            // 
+            // txtSL
+            // 
+            this.txtSL.Location = new System.Drawing.Point(546, 83);
+            this.txtSL.Name = "txtSL";
+            this.txtSL.Size = new System.Drawing.Size(100, 20);
+            this.txtSL.TabIndex = 10;
+            // 
+            // txtDG
+            // 
+            this.txtDG.Location = new System.Drawing.Point(546, 148);
+            this.txtDG.Name = "txtDG";
+            this.txtDG.Size = new System.Drawing.Size(100, 20);
+            this.txtDG.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(336, 37);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Quan Ly Sach";
+            // 
+            // dgv
+            // 
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dgv.Location = new System.Drawing.Point(205, 196);
+            this.dgv.Name = "dgv";
+            this.dgv.Size = new System.Drawing.Size(441, 120);
+            this.dgv.TabIndex = 13;
+            this.dgv.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_CellContentClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Mã sách";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Tên sách";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Số lượng";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Đơn giá";
+            this.Column4.Name = "Column4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtDG);
+            this.Controls.Add(this.txtSL);
+            this.Controls.Add(this.txttenS);
+            this.Controls.Add(this.txtmaS);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnTim);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnThem);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnTim;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtmaS;
+        private System.Windows.Forms.TextBox txttenS;
+        private System.Windows.Forms.TextBox txtSL;
+        private System.Windows.Forms.TextBox txtDG;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+    }
+}
+
---
+++ Quan-Ly-Sach/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Quan_Ly_Sach
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        XmlDocument doc = new XmlDocument();
+        XmlElement root;
+        string fileName = @"F:\cSao\Quan-Ly-Sach\Sach.xml";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            HienThi(dgv);
+        }
+        public void HienThi(DataGridView dgv)// tham so la mot doi tuong data gridview
+        {
+            doc.Load(fileName);// load tệp xml
+            root = doc.DocumentElement;// xác định node gốc
+
+            XmlNodeList ds = root.SelectNodes("sach");// lay danh sách(ds) các node có tên là sach
+            int sd = 0;//lưu chỉ số dòng để hiển thị theo từng dòng trong datagridview
+            foreach (XmlNode item in ds)// duyệt từng node trong danh sách vừa có
+            {
+                dgv.Rows.Add();// tạo 1 dòng trắng trên data gridview
+                dgv.Rows[sd].Cells[0].Value = item.SelectSingleNode("@masach").Value;
+                // lấy giá trị của thuộc tính masach gán vào cột đầu tiên trên dòng thứ sd
+                dgv.Rows[sd].Cells[1].Value = item.SelectSingleNode("tensach").InnerText;
+                dgv.Rows[sd].Cells[2].Value = item.SelectSingleNode("soluong").InnerText;
+                dgv.Rows[sd].Cells[3].Value = item.SelectSingleNode("dongia").InnerText;
+                sd++;// tang số dòng lên để hiển thị node tiếp theo
+            }
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            doc.Load(fileName);// load tệp xml
+            root = doc.DocumentElement;// xác định node gốc
+
+            //tạo nút sach. Do sach có các phần tử con hoặc thuộc tính nên mình phải dung XmlNode.
+            XmlNode sach = doc.CreateElement("sach");
+
+            //tạo nút con của sách là masach
+
+            XmlAttribute masach = doc.CreateAttribute("masach");// tạo 1 attribute nút masach
+            masach.Value = txtmaS.Text;//gán giá trị trên ô textbox txtMS cho node mã sách
+            sach.Attributes.Append(masach);// gán node masach là node con của node sach
+
+            XmlElement tensach = doc.CreateElement("tensach");// tạo 1 element node ten sach
+            tensach.InnerText = txttenS.Text;// gán giá trị trên ô textbox txttenS cho node tensach
+            sach.AppendChild(tensach);//gán node ténach là node con của node sach
+
+            XmlElement soluong = doc.CreateElement("soluong");
+            soluong.InnerText = txtSL.Text;
+            sach.AppendChild(soluong);
+
+            XmlElement dongia = doc.CreateElement("dongia");
+            dongia.InnerText = txtDG.Text;
+            sach.AppendChild(dongia);
+
+            //sau khi tạo xong node sach, thì thêm sach vào gốc root
+            root.AppendChild(sach);
+            doc.Save(fileName);//lưu dữ liệu
+            HienThi(dgv);// hiển thị lại dữ liệu
+
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            doc.Load(fileName);// load tệp xml 
+            root = doc.DocumentElement;// xác định node gốc
+            //láy vị trí cần sửa theo mã sách cũ đưa vào
+            XmlNode sachCu = root.SelectSingleNode("sach[@masach ='" + txtmaS.Text + "']");
+            if (sachCu != null)
+            {
+                // taoj 1 nut sachSuaMoi
+                XmlNode sachSuaMoi = doc.CreateElement("sach");
+
+                //tạo nút con của sách là masach
+                XmlAttribute masach = doc.CreateAttribute("masach");
+                masach.InnerText = txtmaS.Text;//gán giá trị cho mã sách
+                sachSuaMoi.Attributes.Append(masach);
+
+                XmlElement tensach = doc.CreateElement("tensach");
+                tensach.InnerText = txttenS.Text;
+                sachSuaMoi.AppendChild(tensach);
+
+                XmlElement soluong = doc.CreateElement("soluong");
+                soluong.InnerText = txtSL.Text;
+                sachSuaMoi.AppendChild(soluong);
+
+                XmlElement dongia = doc.CreateElement("dongia");
+                dongia.InnerText = txtDG.Text;
+                sachSuaMoi.AppendChild(dongia);
+
+                //thay thế sách cũ bằng sách mới(sửa )
+                root.ReplaceChild(sachSuaMoi, sachCu);
+                doc.Save(fileName);//lưu lại
+                HienThi(dgv);
+            }
+
+        }
+
+        private void dgv_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int t = dgv.CurrentCell.RowIndex;
+            txtmaS.Text = dgv.Rows[t].Cells[0].Value.ToString();
+            txttenS.Text = dgv.Rows[t].Cells[1].Value.ToString();
+            txtSL.Text = dgv.Rows[t].Cells[2].Value.ToString();
+            txtDG.Text = dgv.Rows[t].Cells[3].Value.ToString();
+
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            doc.Load(fileName);// load tệp xml
+            root = doc.DocumentElement;// xác định node gốc
+            XmlNode sachCanXoa = root.SelectSingleNode("sach[@masach ='" + txtmaS.Text + "']");
+            if (sachCanXoa != null)
+            {
+                root.RemoveChild(sachCanXoa);
+
+                doc.Save(fileName);
+
+            }
+            dgv.Rows.Clear();
+            HienThi(dgv);
+
+        }
+
+        private void btnTim_Click(object sender, EventArgs e)
+        {
+            dgv.Rows.Clear();
+            XmlNode sachCanTim = root.SelectSingleNode("sach[@masach ='" + txtmaS.Text.Trim().ToLower() + "']");
+            if (sachCanTim != null)
+            {
+
+                // dgv.Rows.Add();//thêm một dòng mới
+
+                //đưa dữ liệu vào dòng vừa tạo
+                dgv.Rows[0].Cells[0].Value = sachCanTim.SelectSingleNode("@masach").InnerText;
+                dgv.Rows[0].Cells[1].Value = sachCanTim.SelectSingleNode("tensach").InnerText;
+                dgv.Rows[0].Cells[2].Value = sachCanTim.SelectSingleNode("soluong").InnerText;
+                dgv.Rows[0].Cells[3].Value = sachCanTim.SelectSingleNode("dongia").InnerText;
+            }
+
+        }
+
+        
+    }
+}
+
